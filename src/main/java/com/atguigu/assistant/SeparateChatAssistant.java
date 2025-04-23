1d0d403abfb442e869a80ece9f0d242128ce6808
yanjiazheng <1453562554@qq.com>
2025-04-23 22:16:05 +0800

    提示词Prompt功能实现

--- src/main/java/com/atguigu/assistant/SeparateChatAssistant.java
+++ src/main/java/com/atguigu/assistant/SeparateChatAssistant.java
@@ -1,7 +1,9 @@
 package com.atguigu.assistant;
 
 import dev.langchain4j.service.MemoryId;
+import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 
 import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
@@ -20,5 +22,14 @@ public interface SeparateChatAssistant {
      * @param userMessage 用户消息
      * @return
      */
+    @SystemMessage(fromResource = "my-prompt-template.txt")
     String chat(@MemoryId int memoryId, @UserMessage String userMessage);
+
+    @SystemMessage(fromResource = "my-prompt-template3.txt")
+    String chat3(
+            @MemoryId int memoryId,
+            @UserMessage String userMessage,
+            @V("username") String username,
+            @V("age") int age
+    );
 }
---
+++ src/test/java/com/atguigu/PromptTest.java
@@ -0,0 +1,35 @@
+package com.atguigu;
+
+import com.atguigu.assistant.MemoryChatAssistant;
+import com.atguigu.assistant.SeparateChatAssistant;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PromptTest {
+
+    @Autowired
+    private SeparateChatAssistant separateChatAssistant;
+
+    @Test
+    public void testSystemMessage() {
+        String answer = separateChatAssistant.chat(3, "今天几号");
+        System.out.println(answer);
+    }
+
+    @Autowired
+    private MemoryChatAssistant memoryChatAssistant;
+
+    @Test
+    public void testUserMessage() {
+        String answer = memoryChatAssistant.chat("我是环环");
+        System.out.println(answer);
+    }
+
+    @Test
+    public void testUserInfo() {
+        String answer = separateChatAssistant.chat3(1, "我是谁，我多大了", "翠花", 18);
+        System.out.println(answer);
+    }
+}
