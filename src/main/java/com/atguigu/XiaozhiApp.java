7f38a373de3312630f460001b371eb1df4e883b4
yanjiazheng <1453562554@qq.com>
2025-04-23 20:19:54 +0800

    聊天记忆功能实现

--- src/main/java/com/atguigu/XiaozhiApp.java
+++ src/main/java/com/atguigu/XiaozhiApp.java
@@ -1,4 +1,11 @@
 package com.atguigu;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class XiaozhiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(XiaozhiApp.class, args);
+    }
 }
--- src/test/java/com/atguigu/AIServiceTest.java
+++ src/test/java/com/atguigu/AIServiceTest.java
@@ -1,4 +1,34 @@
 package com.atguigu;
 
+import com.atguigu.assistant.Assistant;
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.service.AiServices;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class AIServiceTest {
+
+    @Autowired
+    private QwenChatModel qwenChatModel;
+
+    @Test
+    public void testChat() {
+        // 创建AIService
+        Assistant assistant = AiServices.create(Assistant.class, qwenChatModel);
+        // 调用service的接口
+        String answer = assistant.chat("Hello");
+        System.out.println(answer);
+    }
+
+    @Autowired
+    private Assistant assistant;
+
+    @Test
+    public void testAssistant() {
+        String answer = assistant.chat("大模型是什么");
+        System.out.println(answer);
+    }
+
 }
