ff97a56d5d2fbac6bf2f52c84cafba3802b67461
yanjiazheng <1453562554@qq.com>
2025-04-23 21:53:10 +0800

    持久化聊天记忆功能实现

---
+++ src/test/java/com/atguigu/MongoCrudTest.java
@@ -0,0 +1,50 @@
+package com.atguigu;
+
+import com.atguigu.bean.ChatMessages;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+
+@SpringBootTest
+public class MongoCrudTest {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    /**
+     * 插入文档
+     */
+    @Test
+    public void testInsert() {
+        ChatMessages chatMessages = new ChatMessages();
+        chatMessages.setContent("聊天记录列表");
+        mongoTemplate.insert(chatMessages);
+    }
+
+    /**
+     * 根据id查询文档
+     */
+    @Test
+    public void testFindById() {
+        ChatMessages chatMessages = mongoTemplate.findById("6808ecafef0a146266efacd3",
+                ChatMessages.class);
+        System.out.println(chatMessages);
+    }
+
+    /**
+     * 修改文档
+     */
+    @Test
+    public void testUpdate() {
+        Criteria criteria = Criteria.where("_id").is("6808ecafef0a146266efacd3");
+        Query query = new Query(criteria);
+        Update update = new Update();
+        update.set("content", "新的聊天记录列表");
+        // 修改或新增
+        mongoTemplate.upsert(query, update, ChatMessages.class);
+    }
+}
