a48c0c29350983e126a0973e865c2522d8d74a6a
yanjiazheng <1453562554@qq.com>
2025-04-24 17:19:09 +0800

    优化小智医疗Agent，实现RAG

--- src/main/java/com/atguigu/config/XiaozhiAgentConfig.java
+++ src/main/java/com/atguigu/config/XiaozhiAgentConfig.java
@@ -1,11 +1,21 @@
 package com.atguigu.config;
 
 import com.atguigu.store.MongoChatMemoryStore;
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.rag.content.retriever.ContentRetriever;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class XiaozhiAgentConfig {
@@ -20,5 +30,21 @@ public class XiaozhiAgentConfig {
                 .maxMessages(20)
                 .chatMemoryStore(mongoChatMemoryStore)
                 .build();
+    }
+
+    @Bean
+    ContentRetriever contentRetrieverXiaozhi() {
+        Document document1 = FileSystemDocumentLoader.loadDocument("D:\\JavaProjects\\java-ai\\src\\main\\resources\\knowledge\\医院信息.md");
+        Document document2 = FileSystemDocumentLoader.loadDocument("D:\\JavaProjects\\java-ai\\src\\main\\resources\\knowledge\\科室信息.md");
+        Document document3 = FileSystemDocumentLoader.loadDocument("D:\\JavaProjects\\java-ai\\src\\main\\resources\\knowledge\\神经内科.md");
+        List<Document> documents = Arrays.asList(document1, document2, document3);
+
+        // 使用内存向量存储
+        InMemoryEmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
+        // 使用默认的文档分割器
+        EmbeddingStoreIngestor.ingest(documents, embeddingStore);
+        // 从嵌入存储（EmbeddingStore）里检索和查询内容相关的信息
+        return EmbeddingStoreContentRetriever.from(embeddingStore);
+
     }
 }
